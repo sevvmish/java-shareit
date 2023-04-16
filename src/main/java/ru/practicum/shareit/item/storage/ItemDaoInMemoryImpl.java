cf22a91631360b6024e895285f9852e5734620f0
Krivtsov <sevvmish@gmail.com>
2023-04-17 00:47:30 +0300

    code style

--- src/main/java/ru/practicum/shareit/item/storage/ItemDaoInMemoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemDaoInMemoryImpl.java
@@ -59,7 +59,7 @@ public class ItemDaoInMemoryImpl implements ItemDao {
         List<Item> result = new ArrayList<>();
 
         for (Item item : items.values()) {
-            if (item.getOwner() == userId) {
+            if (item.getOwner().equals(userId)) {
                 result.add(item);
             }
         }
